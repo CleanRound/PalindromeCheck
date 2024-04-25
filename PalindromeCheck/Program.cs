39d93eb5766108ca94fe9edd54732aee09d7ef1a
Олег Луцик <oleglutsik.dev@gmail.com>
2024-04-25 12:03:58 +0300

    Добавьте файлы проекта.

---
+++ PalindromeCheck/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter a number to check if it's a palindrome: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Is the number {number} a palindrome?: {IsPalindrome(number)}");
+    }
+
+    static bool IsPalindrome(int number)
+    {
+        string numStr = number.ToString();
+
+        int start = 0;
+        int end = numStr.Length - 1;
+
+        while (start < end)
+        {
+            if (numStr[start] != numStr[end])
+            {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
